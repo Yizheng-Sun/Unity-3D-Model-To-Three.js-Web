a304f160c76ee427256ae90c48203bec0a76e2b8
Yizheng Sun <yizheng.sun.2000@gmail.com>
2022-08-03 10:35:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
 app.listen(3000, ()=>{
-    console.log('listen on port 3000');
+    console.log('listening on port 3000');
 });
